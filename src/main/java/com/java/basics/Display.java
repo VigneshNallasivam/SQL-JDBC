3ad046453e3cfefcd82e16c3efd4009b57abffe6
VigneshNallasivam <vickyshanestark@gmail.com>
2023-01-11 20:36:07 +0530

    Added : MASTER - Displaying the Welcome Message for Employee Payroll JDBC Program

---
+++ src/main/java/com/java/basics/Display.java
@@ -0,0 +1,11 @@
+package com.java.basics;
+
+public class Display 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Displaying the Welcome Message for Employee Payroll JDBC Simulation");
+	}
+
+}
